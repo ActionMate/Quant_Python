1a42881328648e1144129f704e5c8ab5c31494b5
Diwakar Rana <diwakarrana696@gmail.com>
2026-08-18 20:17:36 +0530

    just fo git

---
+++ python_part_1/Mini_programs/fibonacci_sequence/infinite.py
@@ -0,0 +1,4 @@
+i,c=0,1
+while True:
+    print(c)
+    c,i=c+i,c
--- python_part_1/Normal_learning_stuff/string.py
+++ python_part_1/Normal_learning_stuff/string.py
@@ -1,2 +1,14 @@
-a=['My','name', 'is','diwakar','rana']
-print(' '.join(a))
+import sys
+def DNA_strand(dna):
+    complement_map = {
+        'A': 'T',
+        'T': 'A',
+        'C': 'G',
+        'G': 'C'
+    }
+    complementary_letters = [complement_map[a] for a in dna]
+    result="".join(complementary_letters)
+    sys.stdout.write(result + '\n')
+    return result    
+letter=input('Enter : ').upper()
+DNA_strand(letter)
