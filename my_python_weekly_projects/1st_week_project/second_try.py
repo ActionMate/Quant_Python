a1d013ef94b6e31d5ea399cfd81396f0393b0cf4
Diwakar Rana <diwakarrana696@gmail.com>
2026-08-20 00:38:40 +0530

    dictionary notes

--- my_python_weekly_projects/1st_week_project/second_try.py
+++ my_python_weekly_projects/1st_week_project/second_try.py
@@ -4,10 +4,17 @@
 students = {'Student_1':77, 'Student_2':89, 'Student_3':45, 'Student_4':87,'Student_5':97, 
             'Student_6':82,'Student_7':33, 'Student_8':76,'Student_9':72, 'Student_10':87}
 d = {'': 0}
+def method_1():
+    for k, v in students.items():
+        if v > list(d.values())[0]:
+            d.popitem()
+            d[k] = v
 
-for k, v in students.items():
-    if v > list(d.values())[0]:
-        d.popitem()
-        d[k] = v
+    print(d)
 
-print(d)
+def method_2():
+    for k, v in students.items():
+        if v > list(d.values())[0]:
+            d.clear()
+            d[k] = v
+    print(d)
